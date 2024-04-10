3fe9793c3619cd61b0a3d6ba91ee14a101f4c3cd
Mario Andres <mrendon@anomali.com>
2024-04-10 17:40:48 -0600

    fix issue with @dnd-kit/core and column visibility

--- src/components/columnOrdering.tsx
+++ src/components/columnOrdering.tsx
@@ -1,8 +1,11 @@
 import {
   DndContext,
   DragEndEvent,
+  PointerSensor,
   useDraggable,
   useDroppable,
+  useSensor,
+  useSensors,
 } from "@dnd-kit/core";
 import { Column, Table } from "@tanstack/react-table";
 import { CSSProperties, PropsWithChildren } from "react";
@@ -14,41 +17,50 @@ type Props<T> = {
 };
 
 export default function ColumnOrdering<T>({ tableInstance }: Props<T>) {
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    })
+  );
   function handleDragEnd(event: DragEndEvent) {
     if (event.over && event.over.id === "droppable") {
       return;
     }
   }
   return (
-    // <DndContext onDragEnd={handleDragEnd}>
-    <Droppable>
-      {tableInstance
-        .getAllColumns()
-        .filter(
-          (col) =>
-            col.columnDef.id !== "expander" && col.columnDef.id !== "selection"
-        )
-        .map((col) => (
-          <DraggableColumn key={col.id} column={col} />
-        ))}
-    </Droppable>
-    // </DndContext>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Droppable>
+        {tableInstance
+          .getAllColumns()
+          .filter(
+            (col) =>
+              col.columnDef.id !== "expander" &&
+              col.columnDef.id !== "selection"
+          )
+          .map((col) => (
+            <DraggableColumn key={col.id} column={col} />
+          ))}
+      </Droppable>
+    </DndContext>
   );
 }
 
-type DroppableColumnProps = PropsWithChildren<{}>;
+type DroppableColumnProps = PropsWithChildren<object>;
 
 function Droppable({ children }: DroppableColumnProps) {
-  //   const { setNodeRef } = useDroppable({
-  //     id: "column",
-  //   });
+  const { setNodeRef } = useDroppable({
+    id: "column",
+  });
   const style: CSSProperties = {
     width: "100%",
     height: "100%",
   };
   return (
-    // <div ref={setNodeRef} style={style}>
-    <div style={style}>{children}</div>
+    <div ref={setNodeRef} style={style}>
+      {children}
+    </div>
   );
 }
 
@@ -56,26 +68,24 @@ type DraggableColumnProps<T> = { column: Column<T> };
 
 function DraggableColumn<T>({ column }: DraggableColumnProps<T>) {
   const { getCanHide, getIsVisible, getToggleVisibilityHandler } = column;
-  //   const { attributes, listeners, setNodeRef, transform } = useDraggable({
-  //     id: column.id,
-  //   });
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: column.id,
+  });
 
-  //   const style = transform
-  //     ? {
-  //         transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-  //       }
-  //     : undefined;
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
 
   return (
-    // <div
-    //   ref={setNodeRef}
-    //   className="draggable-column"
-    //   style={style}
-    //   {...listeners}
-    //   {...attributes}
-    // >
-    // <div className="draggable-column" style={style}>
-    <div className="draggable-column">
+    <div
+      ref={setNodeRef}
+      className="draggable-column"
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
       <div className="column-visibility">
         <IndeterminateCheckbox
           {...{
