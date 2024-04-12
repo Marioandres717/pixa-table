174f0ba06ea466881a9054e259ded7000d1b4810
Mario Andres <mrendon@anomali.com>
2024-04-12 01:17:21 -0600

    remove msw  from dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
-import { resolve } from "path";
+import { rimraf } from "rimraf";
+import { resolve, join } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+
+const removeMSW = () => ({
+  name: "remove-msw",
+  closeBundle: async () => {
+    await rimraf(join(__dirname, "dist", "mockServiceWorker.js"));
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
-      name: "MyLib",
-      fileName: "my-lib",
+      name: "pixaTable",
+      fileName: "pixa-table",
     },
     rollupOptions: {
-      external: ["react", "@tanstack/react-table"],
+      external: ["react", "react-dom", "tanstack/react-table"],
       output: {
         globals: {
           react: "React",
-          "@tanstack/react-table": "ReactTable",
+          "react-dom": "ReactDOM",
+          "tanstack/react-table": "ReactTable",
         },
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), removeMSW()],
 });
