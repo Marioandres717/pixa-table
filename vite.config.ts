f9a3cf198312540fefc0d46e1d26c34c6eb69c6a
Mario Andres <mrendon@anomali.com>
2024-04-25 00:37:07 -0600

    build with jsx-dev-runtime

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,15 @@ import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((env) => ({
+  define: {
+    "process.env": {
+      NODE_ENV:
+        env.command === "build"
+          ? { "process.env.NODE_ENV": "'production'" }
+          : { "process.env.NODE_ENV": "'development'" },
+    },
+  },
   plugins: [
     react(),
     libInjectCss(),
@@ -25,8 +33,6 @@ export default defineConfig({
   ],
   build: {
     copyPublicDir: false,
-    sourcemap: "inline",
-    minify: false,
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
       formats: ["es"],
@@ -34,22 +40,14 @@ export default defineConfig({
       fileName: "pixa-table",
     },
     rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "@tanstack/react-table",
-        "react/jsx-runtime",
-        "react/jsx-dev-runtime",
-      ],
+      external: ["react", "react-dom", "@tanstack/react-table"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
           "@tanstack/react-table": "ReactTable",
-          "react/jsx-runtime": "jsx",
-          "react/jsx-dev-runtime": "jsx",
         },
       },
     },
   },
-});
+}));
