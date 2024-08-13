660b0b7f8088a369f603e91add026926bbdc3e8c
Mario Andres <marioarendon2@gmail.com>
2024-08-12 18:12:15 -0600

    fix colors on light mode

--- src/components/pixaTable.tsx
+++ src/components/pixaTable.tsx
@@ -33,7 +33,7 @@ export function PixaTable<TData>({
   return (
     <div
       role="table"
-      className="grid h-full w-full grid-cols-[1fr,32px] grid-rows-[44px_minMax(44px,auto)_44px] overflow-hidden rounded-[4px] border border-solid font-sans text-sm dark:border-black-92.5 dark:bg-black-100 dark:text-black-10"
+      className="grid h-full w-full grid-cols-[1fr,32px] grid-rows-[44px_minMax(44px,auto)_44px] overflow-hidden rounded-[4px] border border-solid bg-black-5 font-sans text-sm dark:border-black-92.5 dark:bg-black-100 dark:text-black-10"
     >
       {!hideHeader && (
         <TableToolbar
