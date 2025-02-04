69545d24dab198958fd5ec361f5c6dd771ebcffc
Mario Andres <marioarendon2@gmail.com>
2025-02-04 17:39:23 -0500

    feat: enhance row cell components with hover visibility for dynamic content; add showOnHover meta property

--- src/components/virtualizedRowCell.tsx
+++ src/components/virtualizedRowCell.tsx
@@ -24,7 +24,6 @@ export function VirtualizedRowCell<TData>({
   const { column, getContext, getValue } = cell;
   const cellTitle =
     String(getValue()) === "undefined" ? column.id : String(getValue());
-
   return (
     <div
       data-id={virtualColumn.key}
@@ -37,12 +36,24 @@ export function VirtualizedRowCell<TData>({
       })}
       className={clsx(
         "pxt-row-border-b absolute left-0 top-0 flex items-center overflow-hidden whitespace-nowrap border-r border-black-20 bg-inherit px-3 py-2 last:border-r-0 hover:z-10 dark:border-black-92.5",
-        { "pxt-pinned-cell": column.getIsPinned() },
+        {
+          "pxt-pinned-cell": column.getIsPinned(),
+        },
         column.columnDef.meta?.className,
         className,
       )}
     >
-      {flexRender(column.columnDef.cell, getContext())}
+      <span
+        className={clsx(
+          "pointer-events-none inline-block group-hover:pointer-events-auto",
+          {
+            "sr-only opacity-0 group-hover:not-sr-only group-hover:opacity-100":
+              column.columnDef.meta?.showOnHover,
+          },
+        )}
+      >
+        {flexRender(column.columnDef.cell, getContext())}
+      </span>
     </div>
   );
 }
